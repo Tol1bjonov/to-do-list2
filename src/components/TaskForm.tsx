2485d9109ffb27e78f34ae3a11702c527a693c6e
Yusufjon Tolibjonov <yusufjontolibjonov3@gmail.com>
2025-03-06 09:41:24 +0500

    feat/Project completed succesfully

--- src/components/TaskForm.tsx
+++ src/components/TaskForm.tsx
@@ -1,5 +1,58 @@
 import { Component } from "react";
-class TaskForm extends Component{
 
+interface IProps {
+    onAddTask: (description: string, deadline: string) => void;
 }
-export default TaskForm
+
+interface IState {
+    description: string;
+    deadline: string;
+}
+
+class TaskForm extends Component<IProps, IState> {
+    constructor(props: IProps) {
+        super(props);
+        this.state = {
+            description: "",
+            deadline: "",
+        };
+    }
+
+    handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        this.setState({ [e.target.name]: e.target.value } as unknown as IState);
+    };
+
+    handleSubmit = () => {
+        if (!this.state.description || !this.state.deadline) return;
+        this.props.onAddTask(this.state.description, this.state.deadline);
+        this.setState({ description: "", deadline: "" });
+    };
+
+    render() {
+        return (
+            <div>
+                <input
+                    type="text"
+                    name="description"
+                    placeholder="Vazifa nomi"
+                    value={this.state.description}
+                    onChange={this.handleChange}
+                    id="description"
+                />
+                <input
+                    type="date"
+                    name="deadline"
+                    value={this.state.deadline}
+                    onChange={this.handleChange}
+                    id="deadline"
+                />
+                <button 
+                id="form-button"
+                onClick={this.handleSubmit}
+                >Qo‘shish</button>
+            </div>
+        );
+    }
+}
+
+export default TaskForm;
--- src/components/TaskList .tsx
+++ src/components/TaskList .tsx
@@ -1,5 +1,34 @@
 import { Component } from "react";
-class TaskList extends Component{
 
+interface Task {
+    id: number;
+    description: string;
+    deadline: string;
+    completed: boolean;
 }
-export default TaskList 
+
+interface IProps {
+    tasks: Task[];
+    onToggleComplete: (taskId: number) => void;
+    onDeleteTask: (taskId: number) => void;
+}
+
+class TaskList extends Component<IProps> {
+    render() {
+        return (
+            <ul >
+                {this.props.tasks.map((task) => (
+                    <li id="wrapper" key={task.id} style={{ textDecoration: task.completed ? "line-through" : "none" }}>
+                        {task.description} ({task.deadline})
+                        <button id="cancel-btn" onClick={() => this.props.onToggleComplete(task.id)}>
+                            {task.completed ? "Bajarildi✅" : "Bajarilmadi❌"}
+                        </button>
+                        <button id="delete-btn" onClick={() => this.props.onDeleteTask(task.id)}> O‘chirish🗑</button>
+                    </li>
+                ))}
+            </ul>
+        );
+    }
+}
+
+export default TaskList;
